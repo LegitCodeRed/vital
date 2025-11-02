80792695df2ccf80dcd1e822711aa3b96c55618e
LegitCodeRed <e.daniel3464@gmail.com>
2025-11-02 13:50:56 +0100

    removed juice

--- headless/JuceLibraryCode/JuceHeader.h
+++ headless/JuceLibraryCode/JuceHeader.h
@@ -1,48 +1,26 @@
-/*
-
-    IMPORTANT! This file is auto-generated each time you save your
-    project - if you alter its contents, your changes may be overwritten!
-
-    This is the header file that your files should include in order to get all the
-    JUCE library headers. You should avoid including the JUCE headers directly in
-    your own source files, because that wouldn't pick up the correct configuration
-    options for your app.
-
-*/
+/* Minimal JUCE header replacement for vital DSP
+ * This header provides minimal definitions needed by vital DSP code
+ * without requiring the full JUCE library.
+ */
 
 #pragma once
 
-#include "AppConfig.h"
+#include <cmath>
+#include <cstdint>
+#include <algorithm>
+#include <memory>
 
-#include <juce_audio_basics/juce_audio_basics.h>
-#include <juce_audio_formats/juce_audio_formats.h>
-#include <juce_core/juce_core.h>
-#include <juce_data_structures/juce_data_structures.h>
-#include <juce_dsp/juce_dsp.h>
-#include <juce_events/juce_events.h>
+// Define JUCE_LEAK_DETECTOR as a no-op since we're not using JUCE's memory leak detection
+#define JUCE_LEAK_DETECTOR(ClassName)
 
+// Define JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR macro
+// This macro disables copy constructor and copy assignment operator
+#define JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ClassName) \
+    ClassName (const ClassName&) = delete; \
+    ClassName& operator= (const ClassName&) = delete; \
+    JUCE_LEAK_DETECTOR(ClassName)
 
-#if defined (JUCE_PROJUCER_VERSION) && JUCE_PROJUCER_VERSION < JUCE_VERSION
- /** If you've hit this error then the version of the Projucer that was used to generate this project is
-     older than the version of the JUCE modules being included. To fix this error, re-save your project
-     using the latest version of the Projucer or, if you aren't using the Projucer to manage your project,
-     remove the JUCE_PROJUCER_VERSION define from the AppConfig.h file.
- */
- #error "This project was last saved using an outdated version of the Projucer! Re-save this project with the latest version to fix this error."
-#endif
-
-#if ! DONT_SET_USING_JUCE_NAMESPACE
- // If your code uses a lot of JUCE classes, then this will obviously save you
- // a lot of typing, but can be disabled by setting DONT_SET_USING_JUCE_NAMESPACE.
- using namespace juce;
-#endif
-
-#if ! JUCE_DONT_DECLARE_PROJECTINFO
-namespace ProjectInfo
-{
-    const char* const  projectName    = "Vital";
-    const char* const  companyName    = "Matt Tytel";
-    const char* const  versionString  = "99999.9.9";
-    const int          versionNumber  = 0x869f0909;
+// Basic type definitions that might be expected
+namespace juce {
+    // Empty namespace to satisfy any juce:: references if they exist
 }
-#endif
